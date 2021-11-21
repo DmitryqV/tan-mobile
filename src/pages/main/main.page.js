64df5aa9f5115e27288de375ed03183100217ef4
Егор <egornesvickij@gmail.com>
2021-11-21 17:14:35 +0500

    added main page

---
+++ src/pages/main/main.page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const Main = () => {
+  return (
+    <View>
+      <Text>Добавте сюда виджеты жля максимально быстрого доступа к важным разделам приложения</Text>
+    </View>
+  )
+}
