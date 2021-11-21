109ced04472ef0d384235a86a11613bc22498430
Егор <egornesvickij@gmail.com>
2021-11-21 21:11:12 +0500

    added navigation drawer

--- app.root.js
+++ app.root.js
@@ -5,19 +5,27 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login, Profile, Main } from './src/pages/index.export.js';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="main" component={Main} options={{ drawerLabel: 'Home' }} />
+      <Drawer.Screen name="profile" component={Profile} />
+    </Drawer.Navigator>
+  );
+}
 
 export const App = () => {
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
       <SafeAreaProvider>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="login" component={Login} options={{ headerShown: false }} />
-            <Stack.Screen name="main" component={Main} options={{ headerShown: false }} />
-            <Stack.Screen name="profile" component={Profile} options={{ headerShown: false }} />
-          </Stack.Navigator>
+          <MyDrawer />
         </NavigationContainer>
       </SafeAreaProvider>
     </SafeAreaView>
