409f69ee52379687f01def01c69e59f2d3fd699b
Егор <egornesvickij@gmail.com>
2021-11-23 12:14:47 +0500

    added new page in navigation

--- src/pages/index.export.js
+++ src/pages/index.export.js
@@ -1,3 +1,4 @@
 export { Login } from './login/login.page';
 export { Profile } from './profile/profile.page';
 export { Main } from './main/main.page';
+export { Evaluations } from './evaluations/evaluations.pages';
