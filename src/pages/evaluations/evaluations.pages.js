9f29ddb33d0197187da7d9eabc414205da63208c
Егор <egornesvickij@gmail.com>
2021-11-23 12:12:33 +0500

    added new page

---
+++ src/pages/evaluations/evaluations.pages.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const Evaluations = () => {
+  return (
+    <View>
+      <Text>ЗАЧЕТНАЯ КНИЖКА</Text>
+    </View>
+  )
+}
