d15600b1694ba5f951504ea5afa6a4c372402b29
Егор <egornesvickij@gmail.com>
2021-11-23 23:09:37 +0500

    added scroll

--- src/pages/evaluations/evaluations.pages.js
+++ src/pages/evaluations/evaluations.pages.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import storage from "../../utils/storage.utils";
 import { getEvaluations } from "../../api/api.get";
 import { Subject } from "../../components/components.export";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const Evaluations = () => {
 
@@ -23,17 +24,27 @@ export const Evaluations = () => {
 
   console.log(dataEvaluations);
   return (
-    <View>
-      {
-        dataEvaluations !== undefined ?
-          dataEvaluations.map((subject) => {
-            return (
-              <Subject props={subject} key={subject.id} />
-            );
-          })
-          :
-          null
-      }
-    </View>
+    <ScrollView>
+      <View>
+        {
+          dataEvaluations !== undefined ?
+            dataEvaluations.map((subject) => {
+              return (
+                <Subject props={subject} key={subject.id} />
+              );
+            })
+            :
+            null
+        }
+      </View>
+    </ScrollView>
   )
 }
+
+// const styles = StyleSheet.create({
+//   profilePage: {
+//     backgroundColor: '#f2f3f8',
+//     flex: 1,
+//     alignItems: 'center',
+//   },
+// })
