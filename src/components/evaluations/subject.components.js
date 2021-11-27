420b8fa1d29dcab5b09aa51586836bbe7900fe0b
Егор <egornesvickij@gmail.com>
2021-11-27 14:44:23 +0500

    update subject componetn

--- src/components/evaluations/subject.components.js
+++ src/components/evaluations/subject.components.js
@@ -4,7 +4,8 @@ import { View, Text, StyleSheet } from "react-native";
 export const Subject = ({ props }) => {
   return (
     <View style={styles.subject}>
-      <Text>{props.title}</Text>
+      <Text style={styles.arrow}>&#5125;</Text>
+      <Text style={styles.title}>{props.title}</Text>
     </View>
   )
 }
@@ -16,6 +17,18 @@ const styles = StyleSheet.create({
     padding: 10,
     borderTopWidth: 1.5,
     borderColor: '#f2f3f8',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
   },
+  arrow: {
+    color: '#5d78ff',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginRight: 10
+  },
+  title: {
+    color: '#595d6e'
+  }
 })
