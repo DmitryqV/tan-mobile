c50877b00aeb42b16e40a6283f78f0ee0d6c00f5
Егор <egornesvickij@gmail.com>
2021-11-27 15:09:40 +0500

    get id afte onpress on subject

--- src/components/evaluations/subject.components.js
+++ src/components/evaluations/subject.components.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+
+function openInfoSubject(id) {
+  console.log(id);
+}
 
 export const Subject = ({ props }) => {
   return (
-    <View style={styles.subject}>
-      <Text style={styles.arrow}>&#5125;</Text>
-      <Text style={styles.title}>{props.title}</Text>
-    </View>
+    <>
+      <Pressable onPress={() => openInfoSubject(props.id)}>
+        <View style={styles.subject}>
+          <Text style={styles.arrow}>&#5125;</Text>
+          <Text style={styles.title}>{props.title}</Text>
+          <Text id={props.id} style={styles.aboba}>  ABOBA</Text>
+        </View>
+      </Pressable>
+    </>
   )
 }
 
