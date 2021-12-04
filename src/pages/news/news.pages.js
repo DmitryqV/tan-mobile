e240e3bce80ab1c2e5fa0c817e3ae4c409bcf13f
Егор <egornesvickij@gmail.com>
2021-12-04 15:58:09 +0500

    added req on page

--- src/pages/news/news.pages.js
+++ src/pages/news/news.pages.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { ScrollView, View, Text } from "react-native";
+import storage from "../../utils/storage.utils";
+import { getNews } from "../../api/api.get";
 
 export const News = () => {
+
+  storage.load({
+    key: 'token',
+    id: 228,
+  })
+    .then(ret => {
+      getNews(ret)
+    })
+    .catch(err => {
+      console.warn(err.message);
+    });
+
   return (
     <ScrollView>
       <View>
