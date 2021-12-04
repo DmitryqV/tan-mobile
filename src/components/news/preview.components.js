b1bdbd1b4705f8a9cb186e8801fe7ce49b741b59
Егор <egornesvickij@gmail.com>
2021-12-04 16:35:53 +0500

    create preview component

---
+++ src/components/news/preview.components.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const PreviewNews = () => {
+  return (
+    <View>
+      <Text>
+        News Title
+      </Text>
+    </View>
+  )
+}
