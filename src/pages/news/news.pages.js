a937569fc277e781d88124d8f53afd658c2415d0
Егор <egornesvickij@gmail.com>
2021-12-04 15:53:02 +0500

    added news page

---
+++ src/pages/news/news.pages.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView, View, Text } from "react-native";
+
+export const News = () => {
+  return (
+    <ScrollView>
+      <View>
+        <Text>NEWS</Text>
+      </View>
+    </ScrollView>
+  )
+}
