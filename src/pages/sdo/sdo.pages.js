b038fef1ac56f9afd4feca8cbbf056ed59f497f0
Егор <egornesvickij@gmail.com>
2021-12-04 13:23:52 +0500

    added request on page

--- src/pages/sdo/sdo.pages.js
+++ src/pages/sdo/sdo.pages.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { View, Text } from "react-native";
+import storage from "../../utils/storage.utils";
+import { getSdoLink } from "../../api/api.get";
 
 export const Sdo = () => {
+
+  storage.load({
+    key: 'token',
+    id: 228,
+  })
+    .then(ret => {
+      getSdoLink(ret)
+    })
+    .catch(err => {
+      console.warn(err.message);
+    });
+
   return (
     <View>
       <Text>СДО</Text>
