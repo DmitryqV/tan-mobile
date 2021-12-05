477d5670fdce98fede4ef17cd51f000864dd274b
Егор <egornesvickij@gmail.com>
2021-12-05 22:37:48 +0500

    added refresh on login page

--- src/pages/profile/profile.page.js
+++ src/pages/profile/profile.page.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useCallback } from "react";
+import { RefreshControl, View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import storage from '../../utils/storage.utils';
 import { getProfile, getStudentInfo } from "../../api/api.get";
 import { UserInfo, SutentInfo } from "../../components/components.export";
 
+const wait = (timeout) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
 export const Profile = ({ onChangeIsLoggedIn }) => {
 
+  const [refreshing, setRefreshing] = useState(false);
   const [dataProfile, onChangeDataProfile] = useState();
   const [studentInfo, onChangeStudentInfo] = useState();
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    onChangeDataProfile();
+    onChangeStudentInfo();
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   if (dataProfile === undefined && studentInfo === undefined) {
     storage.load({
@@ -24,7 +36,14 @@ export const Profile = ({ onChangeIsLoggedIn }) => {
   }
 
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      }
+    >
       <View style={styles.profilePage}>
         {dataProfile !== undefined ?
           <>
