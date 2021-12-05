cbe60bd5479c9b2ece1ddacf39bcc017f191de76
Егор <egornesvickij@gmail.com>
2021-12-05 13:15:10 +0500

    added webview

--- src/api/api.get.js
+++ src/api/api.get.js
@@ -96,7 +96,7 @@ function getSdoLink(token) {
     }
   })
     .then((response) => {
-      console.log(response);
+      console.log(response.data.link);
     })
     .catch((error) => {
       // console.log(error);
--- src/pages/sdo/sdo.pages.js
+++ src/pages/sdo/sdo.pages.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+// import { View, Text } from "react-native";
+import { WebView } from 'react-native-webview';
 import storage from "../../utils/storage.utils";
 import { getSdoLink } from "../../api/api.get";
 
@@ -17,8 +18,6 @@ export const Sdo = () => {
     });
 
   return (
-    <View>
-      <Text>СДО</Text>
-    </View>
+    <WebView source={{ uri: 'https://sdo.pgups.ru' }} />
   )
 }
