186c3f42477de18939749e50d722f5be60b25518
Егор <egornesvickij@gmail.com>
2021-12-08 00:18:13 +0500

    added refresh

--- src/pages/orders/orders.pages.js
+++ src/pages/orders/orders.pages.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { ScrollView, RefreshControl, View } from 'react-native';
 import storage from '../../utils/storage.utils';
 import { getOrders } from '../../api/api.get';
 import { Order } from '../../components/components.export';
 
+const wait = (timeout) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
 export const Oredrs = () => {
 
+  const [refreshing, setRefreshing] = useState(false);
   const [dataOrders, setDataOrders] = useState();
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setDataOrders();
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   if (dataOrders === undefined) {
     storage.load({
@@ -23,7 +34,14 @@ export const Oredrs = () => {
 
   return (
     <>
-      <ScrollView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+      >
         {dataOrders !== undefined ?
           <View>
             {dataOrders.map((order) => {
