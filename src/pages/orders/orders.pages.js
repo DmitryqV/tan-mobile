1ad82be7daf4e74f0f4a775d019818e5495abcd2
Егор <egornesvickij@gmail.com>
2021-12-07 10:39:18 +0500

    added page for orders

---
+++ src/pages/orders/orders.pages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const Oredrs = () => {
+  return (
+    <View>
+      <Text>
+        orders
+      </Text>
+    </View>
+  )
+}
