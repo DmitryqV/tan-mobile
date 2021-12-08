dd38e9573a88cb17ddeb0b0ab5203e7a1fe41477
Егор <egornesvickij@gmail.com>
2021-12-09 00:01:14 +0500

    added select for choose forms

--- src/pages/payment/payment.pages.js
+++ src/pages/payment/payment.pages.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
+import { Select } from '../../components/select.components';
 
 export const Payment = () => {
+
+  const [selectValue, setSelectValue] = useState(null);
+
+  function getSelectValue(value) {
+    setSelectValue(value);
+  }
+
+  const forms = [
+    { title: 'Обучение', id: 1 },
+    { title: 'Ресурсы университета', id: 2 },
+    { title: 'ДФК', id: 3 },
+    { title: 'Общежитие', id: 4 },
+  ];
+
   return (
     <View>
-      <Text>Payment</Text>
+      <Text>Для оплаты введите ваши данные.</Text>
+      <Select props={[forms]} getSelectValue={getSelectValue} />
     </View>
   )
 }
