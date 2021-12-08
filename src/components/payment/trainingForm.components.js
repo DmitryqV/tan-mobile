94659b7fd66a3576c6a6354e1476680613d63b03
Егор <egornesvickij@gmail.com>
2021-12-09 00:33:38 +0500

    added states for inputs value

--- src/components/payment/trainingForm.components.js
+++ src/components/payment/trainingForm.components.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 
 export const TrainingForm = () => {
+
+  const [contractNumber, onChangeContractNumber] = useState('');
+  const [nameCustomer, onChangeNameCustomer] = useState('');
+  const [nameStudent, onChangeNameStudent] = useState('');
+  const [email, onChangeEmail] = useState('');
+  const [paymentAmount, onChangePaymentAmount] = useState('');
   return (
     <View>
       <Text>Номер договора*</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} onChangeText={onChangeContractNumber} />
       <Text>ФИО (заказчик)*</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} onChangeText={onChangeNameCustomer} />
       <Text>ФИО (обучающегося)*</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} onChangeText={onChangeNameStudent} />
       <Text>Электронная почта *</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} onChangeText={onChangeEmail} />
       <Text>Сумма оплаты *</Text>
-      <TextInput style={styles.input} />
+      <TextInput style={styles.input} onChangeText={onChangePaymentAmount} />
 
       <TouchableOpacity>
         <View>
