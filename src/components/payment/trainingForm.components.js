114e5dc7304941c3fe4bd259b55592f72d1f7fd3
Егор <egornesvickij@gmail.com>
2021-12-09 00:08:42 +0500

    create training form

---
+++ src/components/payment/trainingForm.components.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const TrainingForm = () => {
+  return (
+    <View>
+      <Text>Номер договора*</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+
+})
