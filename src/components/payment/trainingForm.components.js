2ce70140c1f703e8915a972571d7442ad811c6f5
Егор <egornesvickij@gmail.com>
2021-12-09 00:28:00 +0500

    added form components

--- src/components/payment/trainingForm.components.js
+++ src/components/payment/trainingForm.components.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 
 export const TrainingForm = () => {
   return (
     <View>
       <Text>Номер договора*</Text>
+      <TextInput style={styles.input} />
+      <Text>ФИО (заказчик)*</Text>
+      <TextInput style={styles.input} />
+      <Text>ФИО (обучающегося)*</Text>
+      <TextInput style={styles.input} />
+      <Text>Электронная почта *</Text>
+      <TextInput style={styles.input} />
+      <Text>Сумма оплаты *</Text>
+      <TextInput style={styles.input} />
+
+      <TouchableOpacity>
+        <View>
+          <Text>Оплатить</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  input: {
+    padding: 0,
+    borderColor: '#000000',
+    borderWidth: 1,
+  }
 })
