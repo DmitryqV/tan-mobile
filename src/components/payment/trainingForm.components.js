359f36070721f89282dfb9ff09d6e9ad003ec38b
Егор <egornesvickij@gmail.com>
2021-12-09 23:00:02 +0500

    update styles for form

--- src/components/payment/trainingForm.components.js
+++ src/components/payment/trainingForm.components.js
@@ -9,18 +9,28 @@ export const TrainingForm = () => {
   const [email, onChangeEmail] = useState('');
   const [paymentAmount, onChangePaymentAmount] = useState('');
   return (
-    <View>
-      <Text>Номер договора*</Text>
-      <TextInput style={styles.input} onChangeText={onChangeContractNumber} />
-      <Text>ФИО (заказчик)*</Text>
-      <TextInput style={styles.input} onChangeText={onChangeNameCustomer} />
-      <Text>ФИО (обучающегося)*</Text>
-      <TextInput style={styles.input} onChangeText={onChangeNameStudent} />
-      <Text>Электронная почта *</Text>
-      <TextInput style={styles.input} onChangeText={onChangeEmail} />
-      <Text>Сумма оплаты *</Text>
-      <TextInput style={styles.input} onChangeText={onChangePaymentAmount} />
+    <View style={styles.form}>
+      <View style={styles.textInput}>
+        <Text>Номер договора*</Text>
+        <TextInput style={styles.input} onChangeText={onChangeContractNumber} />
+      </View>
+      <View style={styles.textInput}>
+        <Text>ФИО (заказчик)*</Text>
+        <TextInput style={styles.input} onChangeText={onChangeNameCustomer} />
+      </View>
+      <View style={styles.textInput}>
+        <Text>ФИО (обучающегося)*</Text>
+        <TextInput style={styles.input} onChangeText={onChangeNameStudent} />
+      </View>
+      <View style={styles.textInput}>
 
+        <Text>Электронная почта *</Text>
+        <TextInput style={styles.input} onChangeText={onChangeEmail} />
+      </View>
+      <View style={styles.textInput}>
+        <Text>Сумма оплаты *</Text>
+        <TextInput style={styles.input} onChangeText={onChangePaymentAmount} />
+      </View>
       <TouchableOpacity>
         <View>
           <Text>Оплатить</Text>
@@ -31,9 +41,19 @@ export const TrainingForm = () => {
 }
 
 const styles = StyleSheet.create({
+  form: {
+    padding: 10,
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '95%'
+  },
   input: {
     padding: 0,
     borderColor: '#000000',
     borderWidth: 1,
+    height: 35,
+    marginTop: 5,
+    marginBottom: 10
   }
 })
