a135b4930d46e7fcc439fe93cacd6ef75ebda155
Егор <egornesvickij@gmail.com>
2021-12-10 01:46:41 +0500

    create fdk form

---
+++ src/components/payment/fdkForm.components.js
@@ -0,0 +1,10 @@
+import React from "react";
+import View from "react-native-gesture-handler/lib/typescript/GestureHandlerRootView";
+
+export const FdkForm = () => {
+  return (
+    <View>
+
+    </View>
+  )
+}
