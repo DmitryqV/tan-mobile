0763ec261e2eac261ab0c4ca95db98b4d61ec0a8
Егор <egornesvickij@gmail.com>
2021-12-10 00:08:23 +0500

    create UniversityResourcesForm

---
+++ src/components/payment/universityResourcesForm.components.js
@@ -0,0 +1,8 @@
+import React from "react";
+import View from "react-native-gesture-handler/lib/typescript/GestureHandlerRootView";
+
+export const UniversityResourcesForm = () => {
+  return (
+    <View></View>
+  )
+}
