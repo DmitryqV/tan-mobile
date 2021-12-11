a0cec0ef9cbfd2348500e164e7ec265f3e3d68aa
Егор <egornesvickij@gmail.com>
2021-12-11 23:29:27 +0500

    create general styles for payment forms

---
+++ src/styles/paymentForms.style.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export const paymentFormsStyles = StyleSheet.create({
+  form: {
+    padding: 10,
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '95%'
+  },
+  inputTitle: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  input: {
+    padding: 0,
+    paddingLeft: 5,
+    height: 35,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 4,
+  },
+  payButton: {
+    width: '65%',
+    height: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4,
+    backgroundColor: '#5d78ff',
+    marginTop: 25,
+    marginBottom: 10,
+    maxWidth: 400
+  },
+  payButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
